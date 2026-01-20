1f15dd96b72590cca29d11c170c2ca0f0bf82b48
k1ng0fTerab1th1a <roman.henbach.work@gmail.com>
2026-01-20 22:29:01 +0200

    create TournamentsController to enable first endpoint

---
+++ WebApi/Controllers/TournamentsController.cs
@@ -0,0 +1,34 @@
+﻿using Application.TournamentService;
+using Application.TournamentService.DTOs;
+using Domain.Player;
+using Domain.Tournament;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TournamentsController(TournamentService service, ILogger<TournamentsController> logger) : ControllerBase
+{
+    private readonly TournamentService _service = service;
+    private readonly ILogger<TournamentsController> _logger = logger;
+
+    [HttpGet("{id}", Name = "GetTournamentById")]
+    public Task Get(TournamentId id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] CreateTournamentRequest createTournamentRequest)
+    {
+        _logger.LogInformation("request received: {name}, {matchwinrule}, {format}", 
+            createTournamentRequest.TournamentName, 
+            createTournamentRequest.MatchWinRule, 
+            createTournamentRequest.Format);
+
+        TournamentId id = await _service.Create(createTournamentRequest, new PlayerId(1));
+
+        return CreatedAtRoute("GetTournamentById", id);
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,41 +1,62 @@
+using Application.Shared;
+using Application.TournamentService;
+using Domain.Tournament;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
+builder.Services.AddTransient<TournamentService>();
+builder.Services.AddTransient<ITournamentRepository, TournRepoMock>();
+builder.Services.AddTransient<IUnitOfWork, UoWMock>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "v1");
+        options.RoutePrefix = "";
+    });
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseAuthorization();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapControllers();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+class TournRepoMock : ITournamentRepository
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public async Task AddAsync(Tournament tournament)
+    {
+        Console.WriteLine("AddAsync called.");
+        await Task.CompletedTask;
+    }
+
+    public Task<Tournament?> GetByIdAsync(TournamentId tournamentId)
+    {
+        Console.WriteLine("GetByIdAsync called.");
+        return Task.FromResult<Tournament?>(null);
+    }
+
+    public async Task RemoveAsync(TournamentId tournamentId)
+    {
+        Console.WriteLine("RemoveAsync called.");
+        await Task.CompletedTask;
+    }
+}
+
+class UoWMock : IUnitOfWork
+{
+    public async Task CommitAsync()
+    {
+        Console.WriteLine("CommitAsync called.");
+        await Task.CompletedTask;
+    }
 }
