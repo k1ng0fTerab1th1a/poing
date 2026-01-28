90eb90047a8ba32bd8f38736041a2007637fe14f
k1ng0fTerab1th1a <roman.henbach.work@gmail.com>
2026-01-28 22:55:46 +0200

    make conString be fetched from config and remove mocks

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,13 +1,14 @@
 using Application.Shared;
 using Application.TournamentService;
-using Domain.Tournament;
 using Infrastructure.Persistence;
 using Infrastructure.Persistence.Tournaments;
 using Microsoft.EntityFrameworkCore;
 using WebApi.Middleware;
 
-string conString = "server=localhost;user=EXAMPLE_USER;password=EXAMPLE_PASSWORD;database=poing_dev";
 var builder = WebApplication.CreateBuilder(args);
+
+string conString = builder.Configuration.GetConnectionString("Default") 
+    ?? throw new InvalidOperationException("Could not access default database connection string.");
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
@@ -44,33 +45,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
-class TournRepoMock : ITournamentRepository
-{
-    public void Add(Tournament tournament)
-    {
-        Console.WriteLine("AddAsync called.");
-        var prop = tournament.GetType().GetProperty(nameof(tournament.Id));
-        prop!.SetValue(tournament, new TournamentId(1));
-    }
-
-    public Task<Tournament?> GetByIdAsync(TournamentId tournamentId)
-    {
-        Console.WriteLine("GetByIdAsync called.");
-        return Task.FromResult<Tournament?>(null);
-    }
-
-    public void Remove(Tournament tournament)
-    {
-        Console.WriteLine("RemoveAsync called.");
-    }
-}
-
-class UoWMock : IUnitOfWork
-{
-    public async Task CommitAsync()
-    {
-        Console.WriteLine("CommitAsync called.");
-        await Task.CompletedTask;
-    }
-}
